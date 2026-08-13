23885689bdcb7c58e20f681cee486d25c1915f7f
SERGIORAMOS010599 <sergior@idttecnologias.mx>
2026-08-12 20:27:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -50,12 +50,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const miCatalogo = new Catalog('dress-container');
 
     // Catálogo Oficial MJ Vestidos (Precios en 0 por el momento)
-    const vestido1 = new Dress(1, 'Pasión Rubí', 'Vino / Tinto', 0, 'img/vestido1.png', 'Elegante vestido con corpiño de encaje floral, finas transparencias y una falda fluida de gran caída.');
-    const vestido2 = new Dress(2, 'Brillo Dorado', 'Oro', 0, 'img/vestido2.png', 'Deslumbrante diseño de lentejuelas ceñido al cuerpo, con delicados tirantes y un sofisticado escote en la espalda.');
-    const vestido3 = new Dress(3, 'Destello Celeste', 'Azul Celeste', 0, 'img/vestido3.png', 'Audaz y elegante diseño con corsé estructurado de transparencias, pedrería lineal y una sensual abertura en la pierna.');
-    const vestido4 = new Dress(4, 'Esmeralda Satín', 'Verde Esmeralda', 0, 'img/vestido4.png', 'Sofisticado vestido de satén con escote en V profundo, cintura enmarcada, falda con vuelo y abertura lateral.');
-    const vestido5 = new Dress(5, 'Noche Azul Real', 'Azul Marino', 0, 'img/vestido5.png', 'Vestido vaporoso con mangas largas semitransparentes, escote en V y un hermoso broche de pedrería en la cintura.');
-    const vestido6 = new Dress(6, 'Obsidiana Glamour', 'Negro', 0, 'img/vestido6.png', 'Imponente vestido negro de lentejuelas con corsé, hombros descubiertos con flecos brillantes y abertura pronunciada.');
+    const vestido1 = new Dress(1, 'Pasión Rubí', 'Vino / Tinto', 450, 'img/vestido1.png', 'Elegante vestido con corpiño de encaje floral, finas transparencias y una falda fluida de gran caída.');
+    const vestido2 = new Dress(2, 'Brillo Dorado', 'Oro', 450, 'img/vestido2.png', 'Deslumbrante diseño de lentejuelas ceñido al cuerpo, con delicados tirantes y un sofisticado escote en la espalda.');
+    const vestido3 = new Dress(3, 'Destello Celeste', 'Azul Celeste', 450, 'img/vestido3.png', 'Audaz y elegante diseño con corsé estructurado de transparencias, pedrería lineal y una sensual abertura en la pierna.');
+    const vestido4 = new Dress(4, 'Esmeralda Satín', 'Verde Esmeralda', 450, 'img/vestido4.png', 'Sofisticado vestido de satén con escote en V profundo, cintura enmarcada, falda con vuelo y abertura lateral.');
+    const vestido5 = new Dress(5, 'Noche Azul Real', 'Azul Marino', 450, 'img/vestido5.png', 'Vestido vaporoso con mangas largas semitransparentes, escote en V y un hermoso broche de pedrería en la cintura.');
+    const vestido6 = new Dress(6, 'Obsidiana Glamour', 'Negro', 450, 'img/vestido6.png', 'Imponente vestido negro de lentejuelas con corsé, hombros descubiertos con flecos brillantes y abertura pronunciada.');
 
     // Agregarlos al catálogo
     miCatalogo.addDress(vestido1);
